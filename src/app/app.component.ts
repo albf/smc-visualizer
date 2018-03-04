8dd2f37ec9e632937cdef6bc4cedc4cd7de0a57c
albf <albf.unicamp@gmail.com>
2018-03-04 05:54:36 -0500

    Canvas, sidebar and initial style
    Add initial styles.css colours and spacing information. Also add a main
    canvas with a sidebar, already with some expected commands.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import "fabric";
+
+declare const fabric: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'SMC Visualizer';
+  private title = 'SMC Visualizer';
+
+  private canvas: any;
+  private canvasWidth = 800;
+  private canvasHeight = 600;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.canvas = new fabric.Canvas('canvas', {
+      hoverCursor: 'pointer',
+      selection: true,
+      selectionBorderColor: 'blue'
+    });
+
+    this._updateCanvasSize();
+
+  }
+
+  _updateCanvasSize() {
+    this.canvas.setWidth(this.canvasWidth);
+    this.canvas.setHeight(this.canvasHeight);
+  }
 }
