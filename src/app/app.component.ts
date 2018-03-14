5a3a0760f6b8bbee4ffe1ac481c5bc3bac8f4729
albf <albf.unicamp@gmail.com>
2018-03-13 21:39:11 -0400

    Graph.ts: separate all jointJS
    Also interpret the initial case for the model, having interface for all
    elements (origin still not used).
    
    Canvas (and fabric) still needs to be removed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import "fabric";
-
-import * as jQuery from 'jquery';
-import * as _ from 'lodash';
-import * as $ from 'backbone';
-//const joint = require('../../../../node_modules/jointjs/dist/joint.js');
-import * as joint from 'jointjs';
+import { Graph } from "./graph";
 
 declare const fabric: any;
 
@@ -39,155 +34,28 @@ export class AppComponent {
     });
     this.canvas.add(add);
 
-
-    let graph = new joint.dia.Graph;
-
-    let paper = new joint.dia.Paper({
-      el: jQuery("#paper"),
-      width: 600,
-      height: 400,
-      model: graph,
-      gridSize: 1
-    });
-
-    let rect = new joint.shapes.basic.Rect({
-      position: { x: 100, y: 30 },
-      size: { width: 100, height: 30 },
-      attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
-    });
-
-    let rect2 = rect.clone();
-    let rect3 = rect.clone();
-    let rect4 = rect.clone();
-    // rect2.translate(300);
-
-    var Link = joint.dia.Link.define('demo.Link', {
-      attrs: {
-          '.connection': {
-              stroke: 'gray',
-              strokeWidth: 2,
-              pointerEvents: 'none',
-              targetMarker: {
-                  type: 'path',
-                  fill: 'gray',
-                  stroke: 'none',
-                  d: 'M 10 -10 0 0 10 10 z'
-              }
-          }
-      },
-      connector: {
-          name: 'rounded'
-      },
-      z: -1,
-      weight: 1,
-      minLen: 1,
-      labelPosition: 'c',
-      labelOffset: 10,
-      labelSize: {
-          width: 50,
-          height: 30
-      },
-      labels: [{
-          markup: '<rect/><text/>',
-          attrs: {
-              text: {
-                  fill: 'gray',
-                  textAnchor: 'middle',
-                  refY: 5,
-                  refY2: '-50%',
-                  fontSize: 20,
-                  cursor: 'pointer'
-              },
-              rect: {
-                  fill: 'lightgray',
-                  stroke: 'gray',
-                  strokeWidth: 2,
-                  refWidth: '100%',
-                  refHeight: '100%',
-                  refX: '-50%',
-                  refY: '-50%',
-                  rx: 5,
-                  ry: 5
-              }
-          },
-          size: {
-              width: 50, height: 30
-          }
-      }]
-  }, {
-    markup: '<path class="connection"/><g class="labels"/>',
-
-    connect: function(sourceId, targetId) {
-        return this.set({
-            source: { id: sourceId },
-            target: { id: targetId }
-        });
-    },
-
-    setLabelText: function(text) {
-        return this.prop('labels/0/attrs/text/text', text || '');
-    }
-});
-
-    /*var link1 = new joint.dia.Link({
-      source: { id: rect.id },
-      target: { id: rect2.id }
-    }); */
-    //var link1 = new Link().connect(rect.id, rect2.id);
-    var attrs = {
-          '.connection': {
-              stroke: 'gray',
-              strokeWidth: 2,
-              pointerEvents: 'none',
-              targetMarker: {
-                  type: 'path',
-                  fill: 'gray',
-                  stroke: 'none',
-                  d: 'M 10 -10 0 0 10 10 z'
-              }
-          }
-      };
-
-    var link1 = new joint.dia.Link({attrs,
-        source: { id: rect.id } ,
-        target: { id: rect2.id}
-    });
-
-    var link2 = new joint.dia.Link({
-      source: { id: rect.id },
-      target: { id: rect3.id }
-    });
-
-    var link3 = new joint.dia.Link({
-      source: { id: rect3.id },
-      target: { id: rect4.id }
-    });
-
-    var link4 = new joint.dia.Link({
-      source: { id: rect.id },
-      target: { id: rect4.id }
-    });
-
-     graph.addCells([rect, rect2, rect3, rect4, link1, link2, link3, link4]);
-
-    joint.layout.DirectedGraph.layout(graph, {
-        setVertices: true,
-        ranker: "longest-path",
-        rankDir: "TB",
-        rankSep: 0,
-        edgeSep: 0,
-        nodeSep: 0
-    });
-    /* joint.layout.DirectedGraph.layout(graph, {
-                setVertices: true,
-                setLabels: true,
-                ranker: "longer-path",
-                rankDir: "TB",
-                align: "UL",
-                rankSep: 0,
-                edgeSep: 0,
-                nodeSep: 0
-            }); */
+    let graph = new Graph();
+    graph.draw({
+        nodes: [
+            {
+                code: "abc",
+                destinations: [1, 3]
+            },
+            {
+                code: "dce",
+                destinations: [2, 3]
+            },
+            {
+                code: "fgh",
+                destinations: [3]
+            },
+            {
+                code: "ijk",
+                destinations: []
+            },
+        ],
+        changes: []
+    })
   }
 
   _updateCanvasSize() {
---
+++ src/app/graph.ts
@@ -0,0 +1,100 @@
+import * as jQuery from 'jquery';
+import * as _ from 'lodash';
+import * as $ from 'backbone';
+import * as joint from 'jointjs';
+
+// Model definitions - later transformed in a jointjs graph
+
+interface TraceNode {
+    code: string,
+    destinations: number[]
+}
+
+interface TraceChange {
+    type: 'modify' | 'remove' | 'add',
+    target: number
+    result: TraceNode
+}
+
+interface Trace {
+    nodes: TraceNode[];
+    changes: TraceChange[];
+}
+
+// Actual graph and helper functions
+
+export class Graph {
+    graph: joint.dia.Graph;
+    paper: joint.dia.Paper;
+
+    constructor() {
+        this.graph = new joint.dia.Graph;
+
+        this.paper = new joint.dia.Paper({
+            el: jQuery("#paper"),
+            width: 800,
+            height: 600,
+            model: this.graph,
+            gridSize: 1
+        });
+    }
+
+    updateLayout(): void {
+        joint.layout.DirectedGraph.layout(this.graph, {
+            setVertices: true,
+            ranker: "longest-path",
+            rankDir: "TB",
+            rankSep: 0, // TODO: find a good configuration here
+            edgeSep: 10,
+            nodeSep: 0
+        });
+    }
+
+    createRect(code: string): joint.shapes.basic.Rect {
+        return new joint.shapes.basic.Rect({
+            position: { x: 100, y: 30 },
+            size: { width: 100, height: 30 }, // TODO: find how to resize based on text
+            attrs: { rect: { fill: 'blue' }, text: { text: code, fill: 'white' } }
+        });
+    }
+
+    createLink(sourceId: string | number, targetId: string | number): joint.dia.Link {
+        let attrs = {
+            '.connection': {
+                stroke: 'gray',
+                strokeWidth: 2,
+                pointerEvents: 'none',
+                targetMarker: {
+                    type: 'path',
+                    fill: 'gray',
+                    stroke: 'none',
+                    d: 'M 10 -10 0 0 10 10 z'
+                }
+            }
+        };
+
+        return new joint.dia.Link({attrs,
+                source: { id: sourceId } ,
+                target: { id: targetId }
+        });
+    }
+
+    draw(trace: Trace): void {
+        let graphElements = [];
+
+        // Frist create nodes
+        for(let n of trace.nodes) {
+            graphElements.push(this.createRect(n.code));
+        }
+
+        // Then, create edges - mandatory to do be after nodes
+        trace.nodes.forEach((n, sourceIndex) => {
+            n.destinations.forEach((targetIndex) => {
+                graphElements.push(this.createLink(graphElements[sourceIndex].id, graphElements[targetIndex].id));
+            })
+        })
+
+        this.graph.addCells(graphElements);
+        this.updateLayout();
+    }
+}
